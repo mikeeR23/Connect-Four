f89511361bc602b87d284492cd706e6cc5e2750b
Mike <Ramirez1815@yahoo.com>
2019-02-15 18:21:33 -0500

    Started working on win conditions

--- connectFour/connectFour.js
+++ connectFour/connectFour.js
@@ -1,5 +1,11 @@
 
+
 var colors = ["red", "blue", "yellow", "purple"];
+
+var userBoolArray = new Array(42)
+var computerBoolArray = new Array(42)
+initializeArray()
+
 
 var playerColor = ''
 var enemyColor = ''
@@ -12,6 +18,17 @@ var table = document.querySelector('#table')
 var td = document.getElementsByTagName('td')
 var resetButton = document.querySelector('#resetGame')
 
+
+
+
+function initializeArray() // Create 2D array
+{
+	for(var i = 0;i < userBoolArray.length;i++)
+	{
+		userBoolArray[i] = false
+		computerBoolArray[i] = false
+	}
+}
 
 // Adds css class that shows the users color when hovering over td elements
 function addCssClass()
@@ -26,6 +43,7 @@ function addCssClass()
 	}
 
 }
+
 
 // After choosing a color, print text 
 function printText()
@@ -52,36 +70,102 @@ function setResetButton()
 	resetButton.style.display = "block"
 }
 
+function calculateComputerMove()
+{
+	var num = Math.floor(Math.random() * td.length) // Assign random td element number
+
+	if(td[num].style.backgroundColor != '') // Check if the spot is already filled
+	{
+		while(td[num].style.backgroundColor != '') // Assign a different td element until it finds one that is empty
+			num = Math.floor((Math.random() * td.length))
+	}
+
+	td[num].style.backgroundColor = enemyColor // Found an empty element 
+	computerBoolArray[num] = true // Show that the enemy is occupying this spot
+}
+
+function checkUserWinCondition()
+{
+	var cnt = 0 // keep track of how many in a row
+	var j = 0
+	var computerCount = 0
+	var checkHorizontal = new Array(6)
+
+	for(var i = 0;i < td.length;i++)
+	{
+		console.log("Td[" + i + "] = " +userBoolArray[i])
+	}
+
+	for(var i = 0;i < checkHorizontal.length;i++) // Horizontal array used to check for 4 in a row horizontal
+		checkHorizontal[i] = false
+
+	for(var i = 0;i < td.length;i++)
+	{
+		if(userBoolArray[i] && cnt == 0) // first node user has placed 
+		{
+			console.log("Ok starting horizontal check at td[" + i + "]")
+			cnt++
+			checkHorizontal[0] = true
+
+			if(i % 7 > 3)
+			{
+				console.log("i is too big")
+				cnt-- // reset back to 0
+				continue
+			}
+			else
+			{
+				console.log("in the right spoot boi")
+				j = i + 1
+				// until we reach a computer node, an unoccupied node, or end of row
+				while(j % 7 != 6 || !userBoolArray[j] || computerBoolArray[j])
+				{
+					if(userBoolArray[j])
+						cnt++
+					
+					j++
+				} 
+
+				if(cnt == 4) // 4 in a row horizontal
+				{
+					console.log("returning true")
+					return true
+				}
+					
+			}
+		}
+
+	}
+}
+
 // Adds event listeners for all td elements in table
 // When user clicks on the td element the background color changes to the players color choice
 document.querySelectorAll('#myTable tbody tr td').forEach(e => e.addEventListener("click", function(){
 
-	if(e.style.backgroundColor == playerColor || e.style.backgroundColor == enemyColor)
+	if(e.style.backgroundColor == '' && playerColor == '')
+		displayMessage.innerHTML = "Choose a color to start the game"
+	else if(e.style.backgroundColor == playerColor || e.style.backgroundColor == enemyColor)
 		displayMessage.innerHTML = "That spot has already been choosen, please choose another spot"
 	else
 	{
 		displayMessage.innerHTML = ""
 		e.style.backgroundColor = playerColor
+		userBoolArray[e] = true // User's color is in this spot
 		playerTurn = 0
 
+		var win = checkUserWinCondition()
+
+		if(win)
+			alert("we fucking did it boy")
+		else
+			console.log("fuck me sideways")
 		calculateComputerMove()
+		
 	}
+
+	//checkComputerWinCondition() // Checks to see if the computer got 4 in a row
 }))
 
-function calculateComputerMove()
-{
-	
-	var num = Math.floor(Math.random() * td.length)
-	console.log("Initiall number is " + num)
-	if(td[num].style.backgroundColor != '')
-	{
-		while(td[num].style.backgroundColor != '')
-			num = Math.floor((Math.random() * td.length))
-	}
-
-	td[num].style.backgroundColor = enemyColor
-	console.log("Placed on " + num)
-}
 
 document.querySelector('#red').addEventListener("click", function(){
 	setPlayerColor("red")
